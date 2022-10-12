f6cd6c9d989899b412f047bf94ffe24c128ade0e
NotCoderGuy <notcoderguy@gmail.com>
2022-10-12 19:39:18 +0530

    GDrive Added

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,25 +25,23 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         try {
-            Storage::extend('google', function($app, $config) {
-                $options = [];
-
-                if (!empty($config['teamDriveId'] ?? null)) {
-                    $options['teamDriveId'] = $config['teamDriveId'];
-                }
-
-                $client = new \Google\Client();
+            Storage::extend('google', function ($app, $config) {
+                $client = new \Google_Client();
                 $client->setClientId($config['clientId']);
                 $client->setClientSecret($config['clientSecret']);
                 $client->refreshToken($config['refreshToken']);
-                
                 $service = new \Google\Service\Drive($client);
+
+                $options = [];
+                if (isset($config['teamDriveId'])) {
+                    $options['teamDriveId'] = $config['teamDriveId'];
+                }
+
                 $adapter = new \Masbug\Flysystem\GoogleDriveAdapter($service, $config['folder'] ?? '/', $options);
                 $driver = new \League\Flysystem\Filesystem($adapter);
-
                 return new \Illuminate\Filesystem\FilesystemAdapter($driver, $adapter);
             });
-        } catch(\Exception $e) {
+        } catch (\Exception $e) {
             // your exception handling logic
         }
     }
